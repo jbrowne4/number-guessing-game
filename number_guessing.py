1bd908d414844fcd0e81f6f476462477812be347
Jade Browne <jade.browne@outlook.com>
2024-12-20 20:06:38 -0500

    breals, guess counter, elifs, strings

--- number_guessing.py
+++ number_guessing.py
@@ -13,8 +13,10 @@ else:
     quit()
 
 random_number = random.randint(0, top_of_range)
+guesses = 0
 
 while True:
+    guesses += 1
     user_guess = input("Make a guess: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
@@ -23,6 +25,11 @@ while True:
         continue
 
     if user_guess == random_number:
-        print("You've got it!")
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number!")
     else:
-        print("You got it wrong.")
+        print("You were below the number!")
+        
+print("You got it in", guesses, "guesses")
